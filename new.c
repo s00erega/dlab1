38a2b9369aeaed2885a8130a465b1609cfa2d9c0
s00erega <s00erega@gmail.com>
2018-12-01 11:49:15 +0300

    privet

---
+++ new.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("GPL");
+/* A couple of parameters that can be passed in: how many
+times we say hello, and to whom */
+static char *whom = "world";
+module_param(whom, charp, 0);
+static int howmany = 1;
+module_param(howmany, int, 0);
+static int __init hello_init(void)
+{
+int i;
+for (i = 0; i < howmany; i++)
+pr_alert("(%d) Hello, %s\n", i, whom);
+return 0;
+}
+static void __exit hello_exit(void)
+{
+pr_alert("Goodbye, cruel %s\n", whom);
+}
+module_init(hello_init);
+module_exit(hello_exit);
